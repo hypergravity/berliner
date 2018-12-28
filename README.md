88c7cb27c629c9fc90525c70d30690ea4f747b6f
hypergravity <bozhang123>
2018-12-28 13:30:09 +0800

    package code

---
+++ README.md
@@ -0,0 +1,17 @@
+## berliner
+Tools for stellar tracks & isochrones.
+
+## author
+Bo Zhang, [bozhang@nao.cas.cn](mailto:bozhang@nao.cas.cn)
+
+## home page
+- [https://github.com/hypergravity/berliner](https://github.com/hypergravity/berliner)
+- [https://pypi.org/project/berliner/](https://pypi.org/project/berliner/)
+
+## install
+- for the latest **stable** version: `pip install myth`
+- for the latest **github** version: `pip install git+git://github.com/hypergravity/myth`
+
+## acknowledgements
+The *ezpadova* package is from Morgan Fouesneau, which could be found in [https://github.com/mfouesneau/ezpadova](https://github.com/mfouesneau/ezpadova) [downloaded on 2018.12.27].
+Since it is not on **PYPI** and could not be installed automatically, I included it here.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='berliner',
+    version='0.0.1',
+    author='Bo Zhang',
+    author_email='bozhang@nao.cas.cn',
+    description='Tools for stellar tracks & isochrones.',  # short description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/hypergravity/berliner',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Astronomy"],
+    package_dir={'berliner': 'berliner',
+                 'ezpadova': 'ezpadova'},
+    package_data={'ezpadova': ['*.json']},
+    include_package_data = True,
+    requires=['numpy', 'scipy', 'emcee', 'regli']
+)
