0a5d50b418143a7c1cb391935cd205f9bddb955e
hypergravity <bozhang123>
2018-12-28 19:12:37 +0800

    minor changes

--- berliner/parsec/_test.py
+++ berliner/parsec/_test.py
@@ -20,20 +20,23 @@ t.show_in_browser()
 #
 Isochrone(t.columns)
 #%%
+""" download a grid of isochrones """
 import numpy as np
 from berliner.parsec.isochrone_grid import get_isochrone_grid
-#    (get_isochrone_grid, interpolate_to_cube, cubelist_to_hdulist,
-#     combine_isochrones, write_isoc_list)
 
 # set grid
 grid_logt = [6, 7., 9]
 grid_feh = [-2.2, -1., 0, 1., 10]
-#grid_mini = np.arange(0.01, 12, 0.01)
+
+grid_logt = np.arange(6, 11, 0.2)
+grid_feh = np.arange(-4, 1, 0.2)
 
 # get isochrones
 isoc_lgage, isoc_feh, isoc_list = get_isochrone_grid(
-    grid_feh, grid_logt, model="parsec12s", phot="sloan", n_jobs=20, verbose=10, silent=True)
+    grid_feh, grid_logt, model="parsec12s", phot="2mass_spitzer_wise", n_jobs=20, verbose=10, silent=True)
 
+#%%
+""" isochrone set """
 from berliner import utils
 isoset = utils.IsochroneSet(isoc_lgage, isoc_feh, isoc_list)
 isoset.isocs
@@ -42,18 +45,22 @@ print(isoset.colnames)
 isoset.post_proc(model="parsec")
 print(isoset.colnames)
 print(isoset[0])
-isoset[0].show_in_browser()
-
+#isoset[0].show_in_browser()
+isoset.vstack()
 #%%
 """ interpolation """
-from berliner.utils.isochrone_interp import isoc_interp
-isoc = isoset[0]
-isoc_interp(isoc, restrictions=(('logG', 0.01), ('logTe', 0.01)),
-            doubling=1.0, mode='linear', 
-            interp_config=(('logG', 'linear'), ('logTe', 'linear')), M_ini='M_ini')
+
+isoset_interp = isoset.isoc_linterp(restrictions=(('logG', 0.01), ('logTe', 0.01)), interp_colnames="all", M_ini='M_ini', n_jobs=1)
+
+print(isoset)
+print(isoset_interp)
 
 #%%
-%pylab qt5
+""" make a prior """
+
+#%%
+# %pylab qt5
+from matplotlib.pyplot import *
 figure()
 for isoc in isoset.isocs:
     plot(isoc["logTe"], isoc["logG"])
@@ -68,6 +75,6 @@ isoc1 = get_one_isochrone(1E9, 0.00152)
 from astropy.table import Table, vstack
 isoc0_ = Table(isoc0.data)
 isoc1_ = Table(isoc1.data)
-vstack([isoc0_, isoc1_.....])
+vstack([isoc0_, isoc1_,])
 
     
