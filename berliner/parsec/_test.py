6284b5c1a5197ed80c92d7b2b0c8f983432c0af4
hypergravity <bozhang123>
2019-01-03 21:31:35 +0800

    added tests for parsec

--- berliner/parsec/_test.py
+++ berliner/parsec/_test.py
@@ -13,12 +13,13 @@ from berliner.parsec import isochrone_grid, Isochrone
 t = parsec.get_one_isochrone_silently(1e9, 0.0152)
 t = parsec.get_one_isochrone(1e9, 0.0152)
 
-t.post_proc("parsec")
+t.post_proc("parsec12s")
 t.pprint()
 print(t)
 t.show_in_browser()
 #
 Isochrone(t.columns)
+
 #%%
 """ download a grid of isochrones """
 import numpy as np
@@ -34,47 +35,3 @@ grid_feh = np.arange(-4, 1, 0.2)
 # get isochrones
 isoc_lgage, isoc_feh, isoc_list = get_isochrone_grid(
     grid_feh, grid_logt, model="parsec12s", phot="2mass_spitzer_wise", n_jobs=20, verbose=10, silent=True)
-
-#%%
-""" isochrone set """
-from berliner import utils
-isoset = utils.IsochroneSet(isoc_lgage, isoc_feh, isoc_list)
-isoset.isocs
-print(isoset.colnames)
-
-isoset.post_proc(model="parsec")
-print(isoset.colnames)
-print(isoset[0])
-#isoset[0].show_in_browser()
-isoset.vstack()
-#%%
-""" interpolation """
-
-isoset_interp = isoset.isoc_linterp(restrictions=(('logG', 0.01), ('logTe', 0.01)), interp_colnames="all", M_ini='M_ini', n_jobs=1)
-
-print(isoset)
-print(isoset_interp)
-
-#%%
-""" make a prior """
-
-#%%
-# %pylab qt5
-from matplotlib.pyplot import *
-figure()
-for isoc in isoset.isocs:
-    plot(isoc["logTe"], isoc["logG"])
-    
-    
-#%%
-from ezpadova import get_one_isochrone
-isoc0 = get_one_isochrone(1E9, 0.0152, model="parsec12s", phot="sloan",)
-isoc1 = get_one_isochrone(1E9, 0.00152)
-
-
-from astropy.table import Table, vstack
-isoc0_ = Table(isoc0.data)
-isoc1_ = Table(isoc1.data)
-vstack([isoc0_, isoc1_,])
-
-    
