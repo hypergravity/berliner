648b44cc8f3ba93049612a5bbe05a4757a6aae93
Bo Zhang <bozhang@nao.cas.cn>
2019-12-25 23:33:32 +0800

    version 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='berliner',
-    version='0.0.8',
+    version='0.0.9',
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description='Tools for stellar tracks & isochrones.',  # short description
