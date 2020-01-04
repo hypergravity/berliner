a2dc5dcf3be418f842c59737d0a072e439a750b2
Bo Zhang <bozhang@nao.cas.cn>
2020-01-04 18:22:29 +0800

    import read_bc* to mist module

--- berliner/mist/__init__.py
+++ berliner/mist/__init__.py
@@ -1,12 +1,9 @@
 from ._track import read_track, read_tracks_ptn
 from ._isochrone import read_isochrones, read_isochrones_ptn
-
+from .bc import read_bc, read_bc_dir, read_bc_dir_one
 
 """
-
 _isochrone.py   read_mist_isochrone:    read MIST isohrones
 _track.py       read_eep_track:         read MIST eep tracks
-
 bc              MIST bolometric correction tools
-
 """
