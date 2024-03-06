5d86dfb5741bddd75eab19ff1b72b40724fa3083
BO ZHANG <bozhang@nao.cas.cn>
2024-03-06 23:17:07 +0800

    move files

--- berliner/mist/__init__.py
+++ berliner/mist/__init__.py
@@ -1,9 +1,9 @@
-from ._track import read_track, read_tracks_ptn
-from ._isochrone import read_isochrones, read_isochrones_ptn, read_isochrones_basic
+from .track import read_track, read_tracks_ptn
+from .isochrone import read_isochrones, read_isochrones_ptn, read_isochrones_basic
 from .bc import read_bc, read_bc_dir, read_bc_dir_one
 
 """
-_isochrone.py   read_mist_isochrone:    read MIST isohrones
-_track.py       read_eep_track:         read MIST eep tracks
+isochrone.py    read_mist_isochrone:    read MIST isohrones
+track.py        read_eep_track:         read MIST eep tracks
 bc              MIST bolometric correction tools
 """
--- berliner/mist/track.py
+++ berliner/mist/track.py
@@ -10,13 +10,15 @@ from joblib import Parallel, delayed
 Zsun = 0.0142857
 
 
-def read_tracks_ptn(ptn="./*.track.eep", colnames=None, n_jobs=-1, verbose=5,
-                    metatable=True):
-    """ read multiple MIST track files """
+def read_tracks_ptn(
+    ptn="./*.track.eep", colnames=None, n_jobs=-1, verbose=5, metatable=True
+):
+    """read multiple MIST track files"""
     fps = np.sort(glob.glob(ptn))
     print("@mist: reading tracks ...")
     track_list = Parallel(n_jobs=n_jobs, verbose=verbose)(
-        delayed(read_track)(fp, colnames) for fp in fps)
+        delayed(read_track)(fp, colnames) for fp in fps
+    )
 
     meta0 = copy.deepcopy(track_list[0].meta)
     meta0.pop("EEPs")
@@ -37,7 +39,7 @@ def read_tracks_ptn(ptn="./*.track.eep", colnames=None, n_jobs=-1, verbose=5,
 
 
 def read_track(fp, colnames=None):
-    """ read MIST eep tracks """
+    """read MIST eep tracks"""
     # read lines
     f = open(fp, "r+")
     s = f.readlines()
@@ -47,21 +49,20 @@ def read_track(fp, colnames=None):
     MESA_revision = re.split(r"\s+", s[1].strip())[-1]
 
     Yinit, Zinit, FeH, aFe, vvcrit = re.split(r"\s+", s[4].strip())[1:]
-    Yinit = np.float(Yinit)
-    Zinit = np.float(Zinit)
-    FeH = np.float(FeH)
-    aFe = np.float(aFe)
-    vvcrit = np.float(vvcrit)
+    Yinit = float(Yinit)
+    Zinit = float(Zinit)
+    FeH = float(FeH)
+    aFe = float(aFe)
+    vvcrit = float(vvcrit)
 
-    initial_mass, N_pts, N_EEP, N_col, phase, type_ = \
-        re.split(r"\s+", s[7].strip())[1:]
-    initial_mass = np.float(initial_mass)
-    N_pts = np.int(N_pts)
-    N_EEP = np.int(N_EEP)
-    N_col = np.int(N_col)
+    initial_mass, N_pts, N_EEP, N_col, phase, type_ = re.split(r"\s+", s[7].strip())[1:]
+    initial_mass = float(initial_mass)
+    N_pts = int(N_pts)
+    N_EEP = int(N_EEP)
+    N_col = int(N_col)
 
     # get eep info
-    EEPs = tuple([np.int(_) for _ in re.split(r"\s+", s[8].strip())[2:]])
+    EEPs = tuple([int(_) for _ in re.split(r"\s+", s[8].strip())[2:]])
     # eep = np.arange(EEPs[0], EEPs[-1] + 1) sometimes inconsistent with data
 
     # add eep column
@@ -71,12 +72,12 @@ def read_track(fp, colnames=None):
     eep_ok = eep[-1] == EEPs[-1] + 1
     t.add_column(Column(eep, "_eep"))
     # _lgmass
-    t.add_column(Column(np.ones(len(t), )*np.log10(initial_mass), "_lgmass"))
+    t.add_column(Column(np.ones(len(t)) * np.log10(initial_mass), "_lgmass"))
     # _lgage
     t.add_column(Column(np.log10(t["star_age"].data), "_lgage"))
     # _feh
-    t.add_column(Column(np.ones(len(t), ) * FeH, "_feh_ini"))
-    t.add_column(Column(t["log_surf_z"]-np.log10(Zsun), "_feh"))
+    t.add_column(Column(np.ones(len(t)) * FeH, "_feh_ini"))
+    t.add_column(Column(t["log_surf_z"] - np.log10(Zsun), "_feh"))
 
     # add meta info
     meta = OrderedDict(
@@ -98,7 +99,7 @@ def read_track(fp, colnames=None):
         EEP1=EEPs[-1],
         EEP1ACT=EEPs[0] + len(t),
         EEPOK=eep_ok,
-        INTERP=("_INTERP" in fp)
+        INTERP=("_INTERP" in fp),
     )
     t.meta = meta
 
@@ -109,5 +110,5 @@ def read_track(fp, colnames=None):
             try:
                 assert colname in t.colnames
             except AssertionError as ae:
-                raise(ae("{} not in track.colnames!!!".format(colname)))
+                raise (ae("{} not in track.colnames!!!".format(colname)))
         return t[colnames]
