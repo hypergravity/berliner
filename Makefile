855fd00e917692d0a084dd9550f5f7bd195a8344
BO ZHANG <bozhang@nao.cas.cn>
2024-03-06 23:18:12 +0800

    tweaks

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
+clean:
+	rm -rf build dist berliner.egg-info
+	rm -rf .coverage .pytest_cache
+
+install: clean
+	pip install .
+	rm -rf build dist berliner.egg-info
+
+uninstall:
+	pip uninstall berliner -y
+
+test:
+	coverage run -m  pytest . --import-mode=importlib --cov-report=html --cov-report=term-missing
+	coverage report -m
+	rm -rf .coverage .pytest_cache
+
 upload:
 	python setup.py sdist bdist_wheel
 	twine upload dist/*
--- setup.py
+++ setup.py
@@ -10,30 +10,30 @@ with open("requirements.txt", "r") as f:
         if not req.startswith("#") and req.__contains__("==")
     ]
 
-
 setuptools.setup(
-    name='berliner',
-    version='0.2.1',
-    author='Bo Zhang',
-    author_email='bozhang@nao.cas.cn',
-    description='Tools for stellar tracks & isochrones.',  # short description
+    name="berliner",
+    version="0.2.1",
+    author="Bo Zhang",
+    author_email="bozhang@nao.cas.cn",
+    description="Tools for stellar tracks & isochrones.",  # short description
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/hypergravity/berliner',
+    url="http://github.com/hypergravity/berliner",
     packages=setuptools.find_packages(),
-    license='MIT',
-    classifiers=["Development Status :: 5 - Production/Stable",
-                 "Intended Audience :: Science/Research",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: OS Independent",
-                 "Programming Language :: Python :: 3.7",
-                 "Topic :: Scientific/Engineering :: Physics",
-                 "Topic :: Scientific/Engineering :: Astronomy"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
     include_package_data=True,  # 设置包含随包数据
     # package_data={  # 具体随包数据路径
     #     "berliner": ["data/*"],
     # },
     install_requires=requirements,
     python_requires=">=3.11",
-
 )
