856a764a70676f3c0e52988b708137b99ad54310
Bo Zhang <bozhang@nao.cas.cn>
2024-03-22 16:23:44 +0800

    deprecate _isochrone_set.py

--- berliner/deprecated/_isochrone_set.py
+++ berliner/deprecated/_isochrone_set.py
@@ -1,8 +1,7 @@
 import numpy as np
-from regli.regli import bisect_interval
 from regli import Regli
-from astropy.table import Table, Column, vstack
-from .isochrone_interp import isoc_interp, isoc_linterp
+from astropy.table import Column, vstack
+from berliner.interpolate.isochrone_interp import isoc_linterp
 
 # ######################## #
 # to evaluate [M/H] from Z #
@@ -12,9 +11,9 @@ from .isochrone_interp import isoc_interp, isoc_linterp
 # parsec
 def eval_mh_parsec(Z):
     ZXsun = 0.0207
-    Y=0.2485+1.78*Z
-    X = 1-Y-Z
-    MH = np.log10(Z/X)-np.log10(ZXsun)
+    Y = 0.2485 + 1.78 * Z
+    X = 1 - Y - Z
+    MH = np.log10(Z / X) - np.log10(ZXsun)
     return MH
 
 
@@ -23,14 +22,14 @@ def eval_zx_mist(Z):
     Yp = 0.249
     Ypsun = 0.2703
     Zpsun = 0.0142
-    Y = Yp + (Ypsun-Yp)/Zpsun*Z
-    X = 1-Y-Z
-    return np.log10(Z/X)
+    Y = Yp + (Ypsun - Yp) / Zpsun * Z
+    X = 1 - Y - Z
+    return np.log10(Z / X)
 
 
 def eval_mh_mist(Z):
     Zpsun = 0.0142
-    return eval_zx_mist(Z)-eval_zx_mist(Zpsun)
+    return eval_zx_mist(Z) - eval_zx_mist(Zpsun)
 
 
 # combined
@@ -42,6 +41,7 @@ def eval_mh(Z, model="parsec"):
     else:
         raise ValueError("@IsochroneSet: invalid model!")
 
+
 # ######################## #
 # isochrone grid
 # ######################## #
@@ -49,16 +49,15 @@ def eval_mh(Z, model="parsec"):
 
 class IsochroneGrid:
     def __init__(self, isoc_lgage, isoc_mhini, isocs, model=None, Zsun=0.0152):
-        """isocs should be a list of astropy.table.Table / Isochrone instances
-        """
+        """isocs should be a list of astropy.table.Table / Isochrone instances"""
         # assert same length
         n_isocs = len(isocs)
         assert len(isoc_lgage) == n_isocs
         assert len(isoc_mhini) == n_isocs
 
         # self.isocs = isocs
-        self.grid_lgage = np.unique(isoc_lgage)             # grid of lg(age)
-        self.grid_mhini = np.unique(isoc_mhini)             # grid of [M/H]ini
+        self.grid_lgage = np.unique(isoc_lgage)  # grid of lg(age)
+        self.grid_mhini = np.unique(isoc_mhini)  # grid of [M/H]ini
         self.r = Regli(self.grid_lgage, self.grid_mhini)
         self.ind_dict = self.r.ind_dict
 
@@ -88,19 +87,24 @@ class IsochroneGrid:
         self.delta_ready = False
 
     def __repr__(self):
-        s = ("==============================================================\n"
-             "* IsochroneGrid [N_isoc={}   N_tot={}]\n"
-             "* lg(Age)   grid[{}]: {}\n"
-             "* [M/H]ini  grid[{}]: {}\n"
-             "==============================================================\n"
-             "Column names:\n"
-             "{}\n"
-             "==============================================================\n"
-             ).format(
-            len(self.isocs), np.sum([len(_) for _ in self]),
-            len(self.grid_lgage), self.grid_lgage,
-            len(self.grid_mhini), self.grid_mhini,
-            self.colnames)
+        s = (
+            "==============================================================\n"
+            "* IsochroneGrid [N_isoc={}   N_tot={}]\n"
+            "* lg(Age)   grid[{}]: {}\n"
+            "* [M/H]ini  grid[{}]: {}\n"
+            "==============================================================\n"
+            "Column names:\n"
+            "{}\n"
+            "==============================================================\n"
+        ).format(
+            len(self.isocs),
+            np.sum([len(_) for _ in self]),
+            len(self.grid_lgage),
+            self.grid_lgage,
+            len(self.grid_mhini),
+            self.grid_mhini,
+            self.colnames,
+        )
         return s
 
     @staticmethod
@@ -119,13 +123,13 @@ class IsochroneGrid:
         self.isocs = isocs
 
     def get_a_isoc(self, lgage, mhini):
-        """ return the closest isoc via [logAge, [M/H]]"""
+        """return the closest isoc via [logAge, [M/H]]"""
         d_isoc = np.abs(self.lgage - lgage) + np.abs(self.mhini - mhini)
         i_isoc = np.argmin(d_isoc)
         return self.isocs[i_isoc]
 
     def post_proc(self, model=None, Zsun=0.0152):
-        """ post-processing of isochrone
+        """post-processing of isochrone
 
         Parameters
         ----------
@@ -141,7 +145,7 @@ class IsochroneGrid:
             for isoc in self.isocs:
                 _lgmini = Column(np.log10(isoc["Mini"]), name="_lgmini")
                 _mhini = Column(eval_mh_parsec(isoc["Zini"]), name="_mhini")
-                _lgage  = Column(isoc["logAge"], name="_lgage")
+                _lgage = Column(isoc["logAge"], name="_lgage")
                 add_columns_safely(isoc, (_lgmini, _lgage, _mhini))
             self.model = model
 
@@ -160,20 +164,32 @@ class IsochroneGrid:
     def __getitem__(self, item):
         return self.isocs[item]
 
-    def isoc_linterp(self, restrictions=(('logG', 0.01), ('logTe', 0.01)),
-                     interp_colnames=('logG', 'logTe'), sampling_factor=1.0,
-                     sampling_mode="linear", Mini='Mini',
-                     n_jobs=-1, verbose=10):
+    def isoc_linterp(
+        self,
+        restrictions=(("logG", 0.01), ("logTe", 0.01)),
+        interp_colnames=("logG", "logTe"),
+        sampling_factor=1.0,
+        sampling_mode="linear",
+        Mini="Mini",
+        n_jobs=-1,
+        verbose=10,
+    ):
         if interp_colnames == "all":
             interp_colnames = self.colnames
 
         from joblib import Parallel, delayed
+
         isoc_interp_ = Parallel(n_jobs=n_jobs, verbose=verbose)(
-            delayed(isoc_linterp)(isoc, restrictions=restrictions,
-                                  sampling_factor=sampling_factor,
-                                  sampling_mode=sampling_mode,
-                                  interp_colnames=interp_colnames,
-                                  Mini=Mini) for isoc in self.isocs)
+            delayed(isoc_linterp)(
+                isoc,
+                restrictions=restrictions,
+                sampling_factor=sampling_factor,
+                sampling_mode=sampling_mode,
+                interp_colnames=interp_colnames,
+                Mini=Mini,
+            )
+            for isoc in self.isocs
+        )
 
         return IsochroneGrid(self.lgage, self.mhini, isoc_interp_)
 
@@ -187,15 +203,21 @@ class IsochroneGrid:
         print("@make_delta: setting deltas for isochrones ...")
         for (i_lgage, i_mhini), i_isoc in self.r.ind_dict.items():
             n_pts = len(self.isocs[i_isoc])
-            self.add_column(self.isocs[i_isoc],
-                            np.ones((n_pts,), float) * _d_lgage[i_lgage],
-                            "_d_age")
-            self.add_column(self.isocs[i_isoc],
-                            np.ones((n_pts,), float) * _d_mhini[i_mhini],
-                            "_d_mhini")
-            self.add_column(self.isocs[i_isoc],
-                            make_delta(self.isocs[i_isoc]["_lgmini"], "log10"),
-                            "_d_mini")
+            self.add_column(
+                self.isocs[i_isoc],
+                np.ones((n_pts,), float) * _d_lgage[i_lgage],
+                "_d_age",
+            )
+            self.add_column(
+                self.isocs[i_isoc],
+                np.ones((n_pts,), float) * _d_mhini[i_mhini],
+                "_d_mhini",
+            )
+            self.add_column(
+                self.isocs[i_isoc],
+                make_delta(self.isocs[i_isoc]["_lgmini"], "log10"),
+                "_d_mini",
+            )
         self.delta_ready = True
 
     def make_prior_sb(self, func_prior=None, qs=["logTe", "logG"]):
@@ -206,27 +228,31 @@ class IsochroneGrid:
                 from berliner.utils.imf import imf
 
                 def func_prior(mhini, lgage, lgmini):
-                    return imf(10 ** lgmini, kind="salpeter")
+                    return imf(10**lgmini, kind="salpeter")
 
             # stack isochrones
             isoc_stacked = self.vstack()
             # evaluate function prior / [IMF in the simplest case]
-            w = func_prior(isoc_stacked["_lgage"], isoc_stacked["_mhini"],
-                           isoc_stacked["_lgmini"])
+            w = func_prior(
+                isoc_stacked["_lgage"], isoc_stacked["_mhini"], isoc_stacked["_lgmini"]
+            )
             # d_volume
-            dv = isoc_stacked["_d_age"] * isoc_stacked["_d_mhini"] * \
-                 isoc_stacked["_d_mini"]
+            dv = (
+                isoc_stacked["_d_age"]
+                * isoc_stacked["_d_mhini"]
+                * isoc_stacked["_d_mini"]
+            )
 
-            return np.array([isoc_stacked[q] for q in qs]).T, np.array(w*dv)
+            return np.array([isoc_stacked[q] for q in qs]).T, np.array(w * dv)
 
     # for histogram
     @staticmethod
     def edge_to_center(edge):
-        return np.array(edge[:-1]+np.diff(edge))
+        return np.array(edge[:-1] + np.diff(edge))
 
     @staticmethod
     def edges_to_centers(edges):
-        return [np.array(edge[:-1]+np.diff(edge)) for edge in edges]
+        return [np.array(edge[:-1] + np.diff(edge)) for edge in edges]
 
     @staticmethod
     def center_to_edge(center):
@@ -256,21 +282,26 @@ def add_columns_safely(t, cols):
 def make_delta(x, scale="linear", loose=True):
     if scale == "linear":
         if loose:
-            xedges = np.concatenate(([1.5 * x[0] - 0.5 * x[1]],
-                                     x[:-1] + np.diff(x) / 2.,
-                                     [1.5 * x[-1] - 0.5 * x[-2]]))
-        else:
             xedges = np.concatenate(
-                ([x[0]], x[:-1] + np.diff(x) / 2., [x[-1]]))
+                (
+                    [1.5 * x[0] - 0.5 * x[1]],
+                    x[:-1] + np.diff(x) / 2.0,
+                    [1.5 * x[-1] - 0.5 * x[-2]],
+                )
+            )
+        else:
+            xedges = np.concatenate(([x[0]], x[:-1] + np.diff(x) / 2.0, [x[-1]]))
         return np.diff(xedges)
     elif scale == "log10":
-        x = 10 ** x
+        x = 10**x
         if loose:
-            xedges = np.concatenate(([1.5 * x[0] - 0.5 * x[1]],
-                                     x[:-1] + np.diff(x) / 2.,
-                                     [1.5 * x[-1] - 0.5 * x[-2]]))
-        else:
             xedges = np.concatenate(
-                ([x[0]], x[:-1] + np.diff(x) / 2., [x[-1]]))
+                (
+                    [1.5 * x[0] - 0.5 * x[1]],
+                    x[:-1] + np.diff(x) / 2.0,
+                    [1.5 * x[-1] - 0.5 * x[-2]],
+                )
+            )
+        else:
+            xedges = np.concatenate(([x[0]], x[:-1] + np.diff(x) / 2.0, [x[-1]]))
         return np.diff(xedges)
-
